049aab12c2513a37dfc8ef44e8d3b4a14bb94121
David Björklund <david.bjorklund@gmail.com>
2018-05-15 16:24:20 +0200

    return value instead of an object

--- index.js
+++ index.js
@@ -3,19 +3,16 @@
 const weightedMedian = (rows, p = 0, r = rows.length - 1) => {
   //base case for single element
   if (p === r) {
-    return rows[p];
+    return rows[p].value;
   }
 
   // base case for two elements
   if (r - p === 1) {
     if (rows[p].weight === rows[r].weight) {
-      return {
-        value: (rows[p].value + rows[r].value) / 2,
-        weight: 0,
-      };
+      return (rows[p].value + rows[r].value) / 2;
     }
 
-    return rows[p].weight > rows[r].weight ? rows[p] : rows[r];
+    return rows[p].weight > rows[r].weight ? rows[p].value : rows[r].value;
   }
 };
 
--- test.js
+++ test.js
@@ -4,21 +4,12 @@ const w = require('./');
 
 describe('Weighted median', () => {
   test('works with single element', () => {
-    expect(w([{ value: 1, weight: 1 }])).toEqual({ value: 1, weight: 1 });
+    expect(w([{ value: 1, weight: 1 }])).toEqual(1);
   });
 
   test('works with 2 elements', () => {
-    expect(w([{ value: 1, weight: 1 }, { value: 2, weight: 1 }])).toEqual({
-      value: 1.5,
-      weight: 0,
-    });
-    expect(w([{ value: 1, weight: 2 }, { value: 2, weight: 1 }])).toEqual({
-      value: 1,
-      weight: 2,
-    });
-    expect(w([{ value: 1, weight: 1 }, { value: 2, weight: 2 }])).toEqual({
-      value: 2,
-      weight: 2,
-    });
+    expect(w([{ value: 1, weight: 1 }, { value: 2, weight: 1 }])).toEqual(1.5);
+    expect(w([{ value: 1, weight: 2 }, { value: 2, weight: 1 }])).toEqual(1);
+    expect(w([{ value: 1, weight: 1 }, { value: 2, weight: 2 }])).toEqual(2);
   });
 });
